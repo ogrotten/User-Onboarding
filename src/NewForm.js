7d8ff968870db3652320052cbdf956259b10782e
Darrin Lowery <lambda@monsto.com>
2019-10-17 18:06:32 -0500

    couple stretches

--- src/NewForm.js
+++ src/NewForm.js
@@ -1,6 +1,11 @@
 import React, { useState } from "react";
 import { withFormik, Form, Field } from "formik";
 import * as Yup from "yup";
+import axios from "axios";
+
+const roles = [
+	"Head", "Tail", "Hat", "Plug"
+];
 
 const NewForm = ({values, touched, errors}) => {
 	return (
@@ -21,22 +26,46 @@ A Submit button to send our form data to the server.
 				{touched.email && errors.email && (<p className="error">{errors.email}</p>)}
 				<br />
 
-				<Field type="password" name="password" placeholder="Password" />
+				<Field type="password" name="password" placeholder="Password" value={values.password || ""}/>
 				<br />
 
+				<Field component="select" name="role">
+					<option>Pick one</option>
+					{ roles.map(e => {
+						return (<option value={e}>{e}</option>)
+					}
+					)}
+				</Field>
 				<label>
-					<Field type="checkbox" name="terms" checked={values.terms} />
+					<Field type="checkbox" name="terms" checked={values.terms || false} />
 					I have read the Terms of Service.
 				</label>
 				<br />
-				<button>Join</button>
+				<button type="submit">Join</button>
 			</Form>
 		</div>
 	);
 };
 
 const FormikNew = withFormik({
-
+	mapPropsToValues({name, email}) {
+		return {
+			name: name || "",
+			email: email || ""
+		};
+	},
+	validationSchema: Yup.object().shape({
+		name: Yup.string().required(),
+		email: Yup.string().required().email(),
+	}),
+	handleSubmit(values, {}) {
+		axios.post('https://reqres.in/api/users', values)
+		.then(res => {
+			console.log(`Data was:`);
+			console.log(res.data);
+		})
+		.catch(err => console.log(`Problem: ${err.response}`));
+	}
 })(NewForm)
 
 export default FormikNew;
